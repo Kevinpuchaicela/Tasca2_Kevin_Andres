576b797b0b0824719d8cbcf18b850ebd54d64e20
Kevin <kpuchaicela@cifpfbmoll.eu>
2021-02-03 18:37:00 +0100

    Creación de clase Tauler

---
+++ com/jaume/penjat/Tauler.java
@@ -0,0 +1,36 @@
+package com.jaume.penjat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tauler {
+    public List<Character> chars = new ArrayList<>();
+    public int intents;
+    public int vidas;
+
+    public List<Character> getChars() {
+        return chars;
+    }
+
+    public void setChars(List<Character> chars) {
+        this.chars = chars;
+    }
+
+    public int getIntents() {
+        return intents;
+    }
+
+    public void setIntents(int intents) {
+        this.intents = intents;
+    }
+
+    public int getVidas() {
+        return vidas;
+    }
+
+    public void setVidas(int vidas) {
+        this.vidas = vidas;
+    }
+
+
+}
